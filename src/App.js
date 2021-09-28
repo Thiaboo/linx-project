6b061776dfe52073c9c551c1d8860d45b8ff3ed3
Thiaboo <thiagodriver7@gmail.com>
2021-09-28 03:50:19 -0300

    Adicionado Barra de pesquisa/Alteraçoes dos planos de fundo/front

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import React from "react";
+
 const api = {
   key: "f61fa59afe9400c4655c8f687f92af2b",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
 function App() {
+
+
+   const dateBuilder = (d) => {
+     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  
+     let day = days[d.getDay()];
+     let date = d.getDate();
+     let month = months[d.getMonth()];
+     let year = d.getFullYear();
+     
+     return `${day} ${date} ${month} ${year}`
+    }
+
   return (
     <div className="app">
       <main>
         <div className="search-box">
-          
+        <input
+          type="text"
+          className="search-bar"
+          placeholder="Search..."
+          />
+        </div>
+        <div className="location-box">
+          <div className="location"> Blumenau, BR</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box"> 
+        <div className="temp">
+          15°c
+        </div>
+        <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
